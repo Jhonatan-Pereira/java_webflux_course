82985d1e493defec7636f829cf1c7aa814a729ee
Jhonatan Pereira <jhonatanfrade.pereira@gmail.com>
2023-06-09 12:04:18 -0300

    feat: add exception handler to object not found

--- src/main/java/br/com/rontec/webfluxcourse/service/UserService.java
+++ src/main/java/br/com/rontec/webfluxcourse/service/UserService.java
@@ -4,9 +4,12 @@ import br.com.rontec.webfluxcourse.entity.User;
 import br.com.rontec.webfluxcourse.mapper.UserMapper;
 import br.com.rontec.webfluxcourse.model.request.UserRequest;
 import br.com.rontec.webfluxcourse.repository.UserRepository;
+import br.com.rontec.webfluxcourse.service.exception.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,10 @@ public class UserService {
     }
 
     public Mono<User> findById(final String id) {
-        return repository.findById(id);
+        return repository.findById(id).switchIfEmpty(Mono.error(
+                new ObjectNotFoundException(
+                        format("Object not found. Id: %s, Type: %s", id, User.class.getSimpleName())
+                )
+        ));
     }
 }
